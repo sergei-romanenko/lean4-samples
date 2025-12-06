897429fcde5f8be9c80812ba7ba4d49225742e70
Sergei Romanenko <sergei.romanenko@supercompilers.ru>
2025-12-06 13:37:05 +0300

    Initial commit.

---
+++ Lean4Samples.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Lean4Samples` library.
+-- Import modules here that should be built as part of the library.
+import Lean4Samples.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Lean4Samples
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
